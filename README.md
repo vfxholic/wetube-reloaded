b871abedba870c6d0f37cf7463debf74cb2181e1
vfxholic <40542712+vfxholic@users.noreply.github.com>
2022-04-12 20:05:59 +0900

    2022/04/12

---
+++ README.md
@@ -0,0 +1,14 @@
+/ -> Home
+/join -> Join
+/login -> Login
+/sarch -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
---
+++ src/controllers/videoController.js
@@ -0,0 +1,45 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 2,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    },
+];
+
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+}
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+}
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    return res.redirect(`/videos/${id}`);
+};
